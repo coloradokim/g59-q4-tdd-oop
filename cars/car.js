ae0ebd8fb7e06b570048dc69fd0cf65f507dc80a
coloradokim <schlesinger.kim@gmail.com>
2017-11-19 17:31:33 -0700

    refactor car solution to use es6 class syntax

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,20 @@
-module.exports = Car
+class Car {
+  constructor(mpg, gallons, odometer, trips) {
+    this.mpg = mpg;
+    this.gallons = 0;
+    this.odometer = 0;
+    this.trips = [];
+  }
 
-function Car(mpg) {
+  fill(gallons){
+    return this.gallons += gallons;
+  }
 
+  drive(distance) {
+    this.gallons -= distance / this.mpg;
+    this.odometer += distance;
+    this.trips.push(`${distance} miles`);
+  }
 }
 
-Car.prototype.fill = function(gallons) {
-
-}
+module.exports = Car;
